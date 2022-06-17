debc09f308d898d88b33e300e789de945486f1ef
Emanuele Padovani <emanuele.padovani@kromin.it>
2022-06-17 14:54:37 +0200

    phases section

--- src/components/TokenSection/index.js
+++ src/components/TokenSection/index.js
@@ -1,8 +1,19 @@
-import React, {useRef} from "react"
+import React from "react"
 import * as classes from './tokenSection.module.scss'
 import GradientLine from "../UI/GradientLine"
 import {sections} from "../../helpers/constants"
 import Background from "../../images/backgrounds/token.mp4"
+import BoxList from "../UI/BoxList"
+import cx from 'classnames'
+import TradingIcon from '../../assets/icn_trading.svg'
+import CollectingIcon from '../../assets/icn_collecting.svg'
+import BettingIcon from '../../assets/icn_betting.svg'
+import BusinessIcon from '../../assets/icn_business.svg'
+import ManagementIcon from '../../assets/icn_management.svg'
+import DesignIcon from '../../assets/icn_design.svg'
+import BuildingIcon from '../../assets/icn_building.svg'
+import BattlingIcon from '../../assets/icn_battling.svg'
+import AchievingIcon from '../../assets/icn_win.svg'
 
 const TokenSection = () => {
     const playPauseVideo = () => {
@@ -18,31 +29,149 @@ const TokenSection = () => {
         <section
             id={sections.TOKEN}
             className={classes.tokenSection}
-            onClick={playPauseVideo}
         >
-            <div className={classes.bgGradient}/>
-            <GradientLine className={classes.greenLine}/>
 
-            <div className="container">
+            <div
+                className={classes.topSection}
+                onClick={playPauseVideo}
+            >
+                <div className={classes.bgGradientTop}/>
+                <div className={classes.bgGradientBottom}/>
+                <GradientLine className={classes.greenLine}/>
+
+                <div className="container">
+                    <div className="row">
+                        <div className="col-12">
+                            <h3>Expand your horizons</h3>
+                            <h4>Collecting our token</h4>
+                            <h3 className={classes.largeMargin}>4 Phases</h3>
+                            <h4>For a unique experience</h4>
+                        </div>
+                    </div>
+                </div>
+                <div
+                    className={classes.background}
+                    dangerouslySetInnerHTML={{
+                        __html: `<video
+                                    id="videoRef"
+                                    muted
+                                    playsInline
+                                    src=${Background}
+                                 />`
+                    }}
+                />
+            </div>
+
+            <div className={cx("container", classes.bottomSection)}>
                 <div className="row">
-                    <div className="col-12">
-                        <h3>Expand your horizons</h3>
-                        <h4>Collecting our token</h4>
+                    <div className="col-12 col-lg-4">
+                        <BoxList
+                            number={'01'}
+                            color={'#33AAA4'}
+                            items={[
+                                {
+                                    icon: <TradingIcon/>,
+                                    title: 'Trading',
+                                    list: <>
+                                        <li><strong>Buy and sell</strong> Helmets, mecha modules, lands and arenas in the NFT Marketplace</li>
+                                    </>
+                                },
+                                {
+                                    icon: <CollectingIcon/>,
+                                    title: 'Collecting',
+                                    list: <>
+                                        <li><strong>Seek</strong> for collectible helmets</li>
+                                    </>
+                                }
+                            ]}
+                        />
+                        <BoxList
+                            number={'02'}
+                            color={'#57D282'}
+                            style={{marginTop: 40}}
+                            items={[
+                                {
+                                    icon: <BettingIcon/>,
+                                    title: 'Betting',
+                                    list: <>
+                                        <li><strong>Reforge Helmets</strong> in forges and pursuit</li>
+                                        <li><strong>Reforge nexus</strong> and <strong>mecha modules</strong> in</li>
+                                    </>
+                                },
+                                {
+                                    icon: <BusinessIcon/>,
+                                    title: 'Business',
+                                    list: <>
+                                        <li><strong>Rent</strong> of all types of industries, lands, mechas or other properties</li>
+                                    </>
+                                }
+                            ]}
+                        />
+                    </div>
+                    <div className="col-12 col-lg-4">
+                        <BoxList
+                            number={'03'}
+                            color={'#5357C3'}
+                            items={[
+                                {
+                                    icon: <ManagementIcon/>,
+                                    title: 'Management',
+                                    list: <>
+                                        <li><strong>Buy industries, manage</strong> and <strong>mine</strong></li>
+                                        <li>
+                                            Create the community and <strong>outline</strong> together the
+                                            <strong>future</strong> of the <strong>Mechaversus world</strong>
+                                        </li>
+                                    </>
+                                },
+                                {
+                                    icon: <DesignIcon/>,
+                                    title: 'Design',
+                                    list: <>
+                                        <li><strong>Design</strong>, craft and sell <strong>new NFTs</strong></li>
+                                    </>
+                                },
+                                {
+                                    icon: <BuildingIcon/>,
+                                    title: 'Building',
+                                    list: <>
+                                        <li>
+                                            <strong>Customise</strong> your <strong>mecha</strong> in the workshop and
+                                            create your "deck" of mechas
+                                        </li>
+                                        <li>Build hangars to repair broken mecha</li>
+                                    </>
+                                }
+                            ]}
+                        />
+                    </div>
+                    <div className="col-12 col-lg-4">
+                        <BoxList
+                            number={'04'}
+                            color={'#A4295A'}
+                            items={[
+                                {
+                                    icon: <BattlingIcon/>,
+                                    title: 'Betting',
+                                    list: <>
+                                        <li><strong>Reforge Helmets</strong> in forges and pursuit</li>
+                                        <li><strong>Reforge nexus</strong> and <strong>mecha modules</strong> in</li>
+                                    </>
+                                },
+                                {
+                                    icon: <AchievingIcon/>,
+                                    title: 'Achieving',
+                                    list: <>
+                                        <li><strong>Achieve</strong> daily and weekly <strong>rewards</strong></li>
+                                        <li><strong>Improve</strong> your Rank and climb the leaderboard</li>
+                                    </>
+                                }
+                            ]}
+                        />
                     </div>
                 </div>
             </div>
 
-            <div
-                className={classes.background}
-                dangerouslySetInnerHTML={{
-                    __html: `<video
-                                id="videoRef"
-                                muted
-                                playsInline
-                                src=${Background}
-                             />`
-                }}
-            />
         </section>
     )
 }
---
+++ src/components/UI/BoxList/index.js
@@ -0,0 +1,53 @@
+import React from "react"
+import PropTypes from "prop-types"
+import * as classes from './boxList.module.scss'
+import cx from 'classnames'
+
+const BoxList = ({
+    number,
+    color,
+    items = [],
+    style,
+    ...rest
+}) => {
+    return (
+        <div
+            className={cx("flexbox-start", classes.boxList)}
+            style={{borderColor: color, ...style}}
+            {...rest}
+        >
+            <div
+                className={classes.middleBorder}
+                style={{borderColor: color}}
+            />
+            <div
+                className={cx("flexbox-center", classes.number)}
+                style={{backgroundColor: color}}
+            >
+                {number}
+            </div>
+
+            {
+                items.map(({icon, title, list}, index) => (
+                    <div>
+                        <span className={cx("flexbox-start" ,classes.title)}>
+                            {icon}
+                            <h5>{title}</h5>
+                        </span>
+                        <ul>
+                            {list}
+                        </ul>
+                    </div>
+                ))
+            }
+        </div>
+    )
+}
+
+export default BoxList
+
+BoxList.propTypes = {
+    number: PropTypes.string,
+    color: PropTypes.string,
+    items: PropTypes.array
+}
