bc1bef2388630a43e491294c3475f6e0a0539b43
Emanuele Padovani <emanuele.padovani@kromin.it>
2022-06-30 11:27:08 +0200

    channel section refactor

--- src/components/ChannelsSection/index.js
+++ src/components/ChannelsSection/index.js
@@ -1,9 +1,9 @@
 import React, {useEffect, useState} from "react"
 import * as classes from './channelsSection.module.scss'
 import WhitepaperIcon from '../../assets/icn_whitepaper.svg'
-import DiscordIcon from '../../assets/icn_discord.svg'
+import FacebookIcon from '../../assets/icn_facebook.svg'
+import TwitterIcon from '../../assets/icn_twitter.svg'
 import TelegramIcon from '../../assets/icn_telegram.svg'
-import EmailIcon from '../../assets/icn_mail.svg'
 import {sections} from "../../helpers/constants"
 import Background from '../../images/generics/bg-Contacts.png'
 import cx from 'classnames'
@@ -11,7 +11,7 @@ import Button from "../UI/Button"
 import DividerMobile from '../../images/generics/divMobile1.svg'
 
 const ChannelsSection = () => {
-    const [emailInput, setEmailInput] = useState("")
+/*    const [emailInput, setEmailInput] = useState("")
     const [formError, setFormError] = useState(null)
 
     useEffect(() => {
@@ -29,7 +29,7 @@ const ChannelsSection = () => {
 
     const onSubmit = (e) => {
         e.preventDefault()
-    }
+    }*/
 
     return (
         <div
@@ -50,7 +50,56 @@ const ChannelsSection = () => {
                         <h3>Are you ready?</h3>
                     </div>
                 </div>
-                <div className={cx("row", classes.boxes)}>
+                <div className={cx("row", classes.channels)}>
+                    <div className={cx("col-auto", classes.channelBox)}>
+                        <h5>Whitepaper</h5>
+                        <div className={cx("flexbox-start", classes.socialsRow)}>
+                            <div>
+                                <WhitepaperIcon/>
+                                <div  className={cx("flexbox-start", classes.buttons)}>
+                                    <Button
+                                        text={'download'}
+                                        //todo: put the whitepaper link below (window.open("https://linktest@test.test"))
+                                        onClick={() => window.open("")}
+                                    />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={cx("col", classes.channelBox)}>
+                        <h5>Join our channels</h5>
+                        <div className={cx("flexbox-center", classes.socialsRow)}>
+                            <div>
+                                <TwitterIcon/>
+                                <Button
+                                    text={'twitter'}
+                                    onClick={() => window.open("https://twitter.com/Mechaversusgame")}
+                                />
+                            </div>
+                            <div>
+                                <FacebookIcon/>
+                                <Button
+                                    text={'facebook'}
+                                    onClick={() => window.open("https://www.facebook.com/Mechaversus")}
+                                />
+                            </div>
+                            <div>
+                                <TelegramIcon/>
+                                <div className={cx("flexbox-start", classes.buttons)}>
+                                    <Button
+                                        text={'telegram chat'}
+                                        onClick={() => window.open("https://t.me/Mechaversus")}
+                                    />
+                                    <Button
+                                        text={'telegram channel'}
+                                        onClick={() => window.open("https://t.me/MechaversusAnn")}
+                                    />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                {/*<div className={cx("row", classes.boxes)}>
                     <div className={cx("col-12 col-md-4 col-xl-auto flexbox-start", classes.actionColumn)}>
                         <h5>Whitepaper</h5>
                         <WhitepaperIcon/>
@@ -60,7 +109,7 @@ const ChannelsSection = () => {
                             onClick={() => window.open("")}
                         />
                     </div>
-                    <div className={cx("col-12 col-md-8 col-xl-auto flexbox-start", classes.actionColumn)}>
+                    <div className={cx("col-12 col-md-8 col-xl flexbox-start", classes.actionColumn)}>
                         <h5>Join our channels</h5>
                         <div className={cx("flexbox-center", classes.centralButtons)}>
                             <div className="flexbox-center">
@@ -109,7 +158,7 @@ const ChannelsSection = () => {
                             />
                         </div>
                     </form>
-                </div>
+                </div>*/}
             </div>
         </div>
     )
