2ca16b963a264d0f41aae87f2c49fb5da3d25ab3
Emanuele Padovani <emanuele.padovani@kromin.it>
2022-06-30 10:25:08 +0200

    input errors

--- src/components/ChannelsSection/index.js
+++ src/components/ChannelsSection/index.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, {useEffect, useState} from "react"
 import * as classes from './channelsSection.module.scss'
 import WhitepaperIcon from '../../assets/icn_whitepaper.svg'
 import DiscordIcon from '../../assets/icn_discord.svg'
@@ -11,6 +11,26 @@ import Button from "../UI/Button"
 import DividerMobile from '../../images/generics/divMobile1.svg'
 
 const ChannelsSection = () => {
+    const [emailInput, setEmailInput] = useState("")
+    const [formError, setFormError] = useState(null)
+
+    useEffect(() => {
+        if(!!emailInput) {
+            const  emailRegex = /^([a-zA-Z0-9_.-])+@(([a-zA-Z0-9-]{2,})+.)+([a-zA-Z0-9]{2,})+$/
+
+            emailRegex.test(emailInput)
+                ? setFormError(null)
+                : setFormError("Enter a valid email!")
+        }
+        else {
+            setFormError(null)
+        }
+    }, [emailInput])
+
+    const onSubmit = (e) => {
+        e.preventDefault()
+    }
+
     return (
         <div
             id={sections.WHITEPAPERS_CHANNELS}
@@ -49,18 +69,33 @@ const ChannelsSection = () => {
                             </div>
                         </div>
                     </div>
-                    <div className={cx("col-12 col-xl flexbox-start", classes.actionColumn)}>
+                    <form
+                        onSubmit={onSubmit}
+                        className={cx("col-12 col-xl flexbox-start", classes.actionColumn)}
+                    >
                         <h5>Subscribe to our newsletter</h5>
                         <EmailIcon/>
                         <div className={cx("flexbox-center", classes.submit)}>
-                            <input
-                                type="text"
-                                placeholder="Email address"
-                                className={classes.inputText}
+                            <div className={classes.inputWrapper}>
+                                <input
+                                    name="email"
+                                    type="text"
+                                    placeholder="Email address"
+                                    autoComplete="off"
+                                    onChange={({target: input}) => setEmailInput(input.value)}
+                                    className={classes.inputText}
+                                    style={{color: !!formError ? "#FF113A" : "#FFF"}}
+                                />
+                                <small>{formError}</small>
+                            </div>
+
+                            <Button
+                                type="submit"
+                                text="Submit"
+                                disabled={!!formError || !emailInput}
                             />
-                            <Button text={'Submit'}/>
                         </div>
-                    </div>
+                    </form>
                 </div>
             </div>
         </div>
--- src/components/UI/Button/index.js
+++ src/components/UI/Button/index.js
@@ -4,13 +4,19 @@ import cx from 'classnames'
 import PropTypes from "prop-types"
 
 const Button = ({
+    type= "submit",
     text,
     onClick,
+    disabled,
+    ...rest
 }) => {
     return (
         <button
+            type={type}
             className={cx("flexbox-center", classes.button)}
             onClick={onClick}
+            disabled={disabled}
+            {...rest}
         >
             {text}
 
@@ -26,6 +32,8 @@ const Button = ({
 export default Button
 
 Button.propTypes = {
+    type: PropTypes.string,
     text: PropTypes.string,
-    onClick: PropTypes.func
+    onClick: PropTypes.func,
+    disabled: PropTypes.bool
 }
