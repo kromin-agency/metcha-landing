0cde620e86d1b8a27f8e9e650586c36488cb802e
Emanuele Padovani <emanuele.padovani@kromin.it>
2022-07-12 12:53:57 +0200

    partner logos placeholders

--- src/components/PartnerSection/index.js
+++ src/components/PartnerSection/index.js
@@ -4,6 +4,16 @@ import {sections} from "../../helpers/constants"
 import cx from 'classnames'
 import Divider from '../../images/generics/div3.svg'
 import DividerMobile from '../../images/generics/divMobile1.svg'
+import Image from "../UI/Image"
+
+//todo: put real partner png logos path into the array below
+const partners = [
+    //e.g. "generics/partner1.png"
+    "logo-variants/LogoIcon_mechaversus_neg.png",
+    "logo-variants/LogoIcon_mechaversus_neg.png",
+    "logo-variants/LogoIcon_mechaversus_neg.png",
+    "logo-variants/LogoIcon_mechaversus_neg.png"
+]
 
 const PartnerSection = ({scrollFromTop, setCurrentSectionCb}) => {
     const sectionRef = useRef()
@@ -42,6 +52,13 @@ const PartnerSection = ({scrollFromTop, setCurrentSectionCb}) => {
                     <div className="col-12">
                         <h3>Partner</h3>
                     </div>
+                </div>
+                <div className={cx("row justify-content-center align-items-center", classes.logos)}>
+                    {partners.map(logo => (
+                        <div className="col-12 col-sm-6 col-md-4 col-lg-3">
+                            <Image alt="partner logo" filename={logo} className={classes.singleLogo}/>
+                        </div>
+                    ))}
                 </div>
             </div>
         </div>
